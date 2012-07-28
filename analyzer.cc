e3edafe5c9b1169f0cf641e26a72191e1df55412
Justin Hugon <>
2012-07-28 21:15:21 +0200

    changes from use

--- analyzer.cc
+++ analyzer.cc
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <TFile.h>
 #include <TTree.h>
+#include <TChain.h>
 #include <TH2F.h>
 #include <TH1F.h>
 #include <TProfile.h>
@@ -15,13 +16,16 @@
 
 #include "PhysicsObjectFunctions.h"
 
+using namespace std;
+
 double getDiM(TClonesArray*, double maxEta);
+double getDeltaEta(TClonesArray* particles, double maxEta);
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2) 
+  if (argc < 2) 
   {
-	std::cout << "testReader requires 1 argument:" << std::endl
+	std::cout << "testReader requires >=1 argument:" << std::endl
 	<< "testReader <infilename.root>" << std::endl;
 	return 1;
   }
@@ -29,10 +33,12 @@ int main(int argc, char *argv[])
   gStyle->SetOptStat(0);
 
   TCanvas* c1 = new TCanvas("c1");
-  TFile* f = new TFile(argv[1]);
-  TTree * tree = (TTree*) f->Get("tree");
-  TH2F* htemp;
-  TH1D *htemp_2;
+  TChain * tree = new TChain("tree");
+
+  for(unsigned iarg=1; iarg<argc;iarg++)
+  {
+    tree->AddFile(argv[iarg]);
+  }
 
   TClonesArray* muons = new TClonesArray("TParticle");
   TClonesArray* electrons = new TClonesArray("TParticle");
@@ -56,100 +62,64 @@ int main(int argc, char *argv[])
   tree->SetBranchAddress("trueMetPhi",&trueMetPhi);
   tree->SetBranchAddress("trueSumPt",&trueSumPt);
 
-  TH1F* muRelIso = new TH1F("muRelIso","Relative Isolation",20,0,2);
-  TH1F* elRelIso = new TH1F("elRelIso","Relative Isolation",20,0,2);
-  TH1F* phRelIso = new TH1F("phRelIso","Relative Isolation",20,0,2);
-
   TH1F* mDiMu = new TH1F("mDiMu","DiMuon Mass",50,0,200);
   TH1F* mDiEl = new TH1F("mDiEl","DiElectron Mass",50,0,200);
   TH1F* mDiJet = new TH1F("mDiJet","DiJet Mass",50,0,200);
   TH1F* mDiPh = new TH1F("mDiPh","DiPhoton Mass",50,0,200);
+
+  TH1F* ptMu1 = new TH1F("ptMu1","Leading Muon Pt",50,0,200);
+  TH1F* ptMu2 = new TH1F("ptMu2","Sub-Leading Muon Pt",50,0,200);
+  TH1F* ptJet1 = new TH1F("ptJet1","Leading Jet Pt",50,0,200);
+  TH1F* ptJet2 = new TH1F("ptJet2","Sub-Leading Jet Pt",50,0,200);
+
+  TH1F* deltaEtaJets = new TH1F("deltaEtaJets","#Delta#eta Jets",50,-10.0,10.0);
   
   unsigned nLightJets = 0;
   unsigned nBJets = 0;
   unsigned nBTagLightJets = 0;
   unsigned nBTagBJets = 0;
   unsigned nEvents = tree->GetEntries();
+  cout << "nEvents: " << nEvents << endl;
   for(unsigned i=0; i<nEvents;i++)
   {
     tree->GetEvent(i);
+    if (i % 1000 == 0) cout << "Event: " << i << endl;
 
     unsigned nMuons = muons->GetEntries();
     unsigned nElectrons = electrons->GetEntries();
     unsigned nPhotons = photons->GetEntries();
     unsigned nJets = jets->GetEntries();
 
+/*
     for(unsigned i=0; i<nMuons; i++)
     {
 	TParticle* tmpMuon = (TParticle*) muons->At(i);
-	muRelIso->Fill(getIso(tmpMuon)/tmpMuon->Pt());
+	float relIso = getIso(tmpMuon)/tmpMuon->Pt();
     }
     for(unsigned i=0; i<nElectrons; i++)
     {
 	TParticle* tmpEl = (TParticle*) electrons->At(i);
-	elRelIso->Fill(getIso(tmpEl)/tmpEl->Pt());
+	float relIso = getIso(tmpEl)/tmpEl->Pt();
     }
     for(unsigned i=0; i<nPhotons; i++)
     {
 	TParticle* tmpPh = (TParticle*) photons->At(i);
-	phRelIso->Fill(getIso(tmpPh)/tmpPh->Pt());
+	float relIso = getIso(tmpPh)/tmpPh->Pt();
     }
     for(unsigned i=0; i<nJets; i++)
     {
 	bool isB = getGenB((TParticle*) jets->At(i));
 	bool isBTag = getBTag((TParticle*) jets->At(i));
-	if(isB)
-	{
-	  nBJets++;
-	  if(isBTag)
-	    nBTagBJets++;
-	}
-	else
-	{
-	  nLightJets++;
-	  if(isBTag)
-	    nBTagLightJets++;
-	}
     }
+*/
 	
 
-    if(nMuons>1)
-        mDiMu->Fill(getDiM(muons,2.4));
-    if(nElectrons>1)
-        mDiEl->Fill(getDiM(electrons,2.5));
-    if(nPhotons>1)
-        mDiPh->Fill(getDiM(photons,2.5));
-    if(nJets>1)
-        mDiJet->Fill(getDiM(jets,5.0));
-/*
-    getBTag();
-    getGenB();
-    getCaloIso();
-    getTrkIso();
-*/
+    mDiMu->Fill(getDiM(muons,2.4));
+    mDiEl->Fill(getDiM(electrons,2.5));
+    mDiPh->Fill(getDiM(photons,2.5));
+    mDiJet->Fill(getDiM(jets,5.0));
+    deltaEtaJets->Fill(getDeltaEta(jets,5.0));
   }
-
-  muRelIso->SetLineColor(kRed+1);
-  elRelIso->SetLineColor(kGreen+1);
-  phRelIso->SetLineColor(kYellow+1);
-  
-  muRelIso->GetXaxis()->SetTitle("Relative Isolation");
-  muRelIso->GetYaxis()->SetTitle("Counts");
-  muRelIso->Draw();
-  elRelIso->Draw("same");
-  phRelIso->Draw("same");
-  TLegend* isoLegend = new TLegend(0.7,0.70,0.9,0.9);
-  isoLegend->SetFillColor(kWhite);
-  isoLegend->AddEntry(muRelIso,"#mu Iso","l");
-  isoLegend->AddEntry(elRelIso,"e Iso","l");
-  isoLegend->AddEntry(phRelIso,"#gamma Iso","l");
-  isoLegend->Draw("same");
-  c1->SaveAs("iso.png");
-
-  std::cout << "\n##########################\nb-Tags: \n";
-  std::cout << "Mistag Rate: " << float(nBTagLightJets)/float(nLightJets)<< std::endl;
-  std::cout << "BTag Eff:    " << float(nBTagBJets)/float(nBJets)<< 
-							std::endl << std::endl;
 
   c1->Clear();
   mDiMu->Draw();
@@ -163,6 +133,17 @@ int main(int argc, char *argv[])
   c1->Clear();
   mDiPh->Draw();
   c1->SaveAs("mDiPh.png");
+  ptMu1->Draw();
+  c1->SaveAs("ptMu1.png");
+  ptMu2->Draw();
+  c1->SaveAs("ptMu2.png");
+  ptJet1->Draw();
+  c1->SaveAs("ptJet1.png");
+  ptJet2->Draw();
+  c1->SaveAs("ptJet2.png");
+
+  deltaEtaJets->Draw();
+  c1->SaveAs("deltaEtaJets.png");
 
   return 0;
 }
@@ -197,4 +178,31 @@ double getDiM(TClonesArray* particles, double maxEta)
         part0->Momentum(p0);
         part1->Momentum(p1);
         return (p0+p1).M();
+}
+
+double getDeltaEta(TClonesArray* particles, double maxEta)
+{
+        if(particles->GetEntries() < 2)
+            return -999999.0;
+        TParticle* part0 = NULL;
+        TParticle* part1 = NULL;
+        for(unsigned i=0; i< particles->GetEntries();i++)
+        {
+            TParticle * tmpPart = (TParticle*) particles->At(0);
+            if(fabs(tmpPart->Eta())<maxEta)
+            {
+                if(part0 == NULL)
+                {
+                    part0 = tmpPart;
+                }
+                else if(part1 == NULL)
+                {
+                    part1 = tmpPart;
+                    break;
+                }
+            }
+        }
+        if(part0 == NULL || part1 == NULL)
+            return -999999.0;
+        return part0->Eta() - part1->Eta();
 }
