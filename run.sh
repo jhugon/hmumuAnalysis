b9eb19e68bc4926163214e5277a8d08da7449d9e
Justin Hugon <jhugon@uftrig01>
2012-09-11 14:12:46 +0200

    changed run.sh to use new syntax for ./analyzer, and also new files

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 nice scons -j4
 
-#TRAININGTREES="true"
-#TRAIN="true"
+TRAININGTREES="true"
+TRAIN="true"
 
 echo "#######################"
-echo "   Running Analizer"
+echo "   Running Analyzer"
 echo "#######################"
 echo `date`
 
@@ -14,10 +14,10 @@ if [ "$TRAININGTREES" = "true" ]; then
 echo "#######################"
 echo "Creating Training Trees"
 echo "#######################"
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/dataPU/ggHmumu125.root  ggHmumu125.root --trainingTree signalTreeGG.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/dataPU/vbfHmumu125.root vbfHmumu125.root --trainingTree signalTreeVBF.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/backgroundNtuples/NtuplesMCDYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1.root  DYJetsToLL.root --trainingTree backgroundTreeDY.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/backgroundNtuples/NtuplesMCTTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S7_START52_V9-v1.root ttbar.root --trainingTree backgroundTreeTT.root
+nice ./analyzer ggHmumu125.root /data/uftrig01b/jhugon/hmumu/privateSignalV2/ggHmumu125.root --trainingTree signalTreeGG.root
+nice ./analyzer vbfHmumu125.root /data/uftrig01b/jhugon/hmumu/privateSignalV2/vbfHmumu125.root --trainingTree signalTreeVBF.root
+nice ./analyzer DYJetsToLL.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesMCDYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1/*.root --trainingTree backgroundTreeDY.root
+nice ./analyzer ttbar.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_2_6/NtuplesMCTTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S7_START52_V9-v1/*.root --trainingTree backgroundTreeTT.root
 echo "#######################"
 echo "#######################"
 fi
@@ -36,10 +36,20 @@ echo "#######################"
 fi
 
 # Run with full MVA
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/dataPU/ggHmumu125.root  ggHmumu125.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/dataPU/vbfHmumu125.root vbfHmumu125.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/backgroundNtuples/NtuplesMCDYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1.root  DYJetsToLL.root
-nice ./analyzer /data/uftrig01b/jhugon/hmumu/backgroundNtuples/NtuplesMCTTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S7_START52_V9-v1.root ttbar.root
+nice ./analyzer ggHmumu125.root /data/uftrig01b/jhugon/hmumu/privateSignalV2/ggHmumu125.root
+nice ./analyzer vbfHmumu125.root /data/uftrig01b/jhugon/hmumu/privateSignalV2/vbfHmumu125.root
+nice ./analyzer DYJetsToLL.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesMCDYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1/*.root
+nice ./analyzer ttbar.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_2_6/NtuplesMCTTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S7_START52_V9-v1/*.root
+
+nice ./analyzer SingleMuRun2012Av1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataSingleMuRun2012A-13Jul2012-v1/*.root
+nice ./analyzer SingleMuRun2012Bv1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataSingleMuRun2012B-13Jul2012-v1/*.root
+nice ./analyzer SingleMuRun2012Cv1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataSingleMuRun2012C-PromptReco-v1/*.root
+nice ./analyzer SingleMuRun2012Cv2.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataSingleMuRun2012C-PromptReco-v2/*.root
+
+nice ./analyzer DoubleMuRun2012Av1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataDoubleMuRun2012A-13Jul2012-v1/*.root
+nice ./analyzer DoubleMuRun2012Bv1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataDoubleMuRun2012B-13Jul2012-v4/*.root
+nice ./analyzer DoubleMuRun2012Cv1.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataDoubleMuRun2012C-PromptReco-v1/*.root
+nice ./analyzer DoubleMuRun2012Cv2.root /data/uftrig01b/digiovan/root/higgs/CMSSW_5_3_3_patch3/NtuplesDataDoubleMuRun2012C-PromptReco-v2/*.root
 
 echo "#######################"
 echo "#######################"
