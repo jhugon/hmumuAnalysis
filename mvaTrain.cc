59535f2766e75dda95668c854813446ca2fe6b4d
Justin Hugon <jhugon@uftrig01>
2013-05-16 11:30:35 +0200

    Moving toward Baseline++ Only changed mvaTrain.cc to use the old vbfPresel+ptMiss<40 for vbf BDT

--- mvaTrain.cc
+++ mvaTrain.cc
@@ -430,9 +430,9 @@ int main(int argc, char *argv[])
 
     std::string cuts;
     if (optionMap.count("vbfSelection") && optionMap["vbfSelection"].as<int>() == 1)
-      cuts = "nJets >= 2 && ptmiss < 100.";
+      cuts = "ptMu1>25. && ptMu2>25. && (mDiJet>300.0 && deltaEtaJets>3.0 && productEtaJets<0.0) && ptmiss<40.";
     else
-      cuts = "nJets < 2";
+      cuts = "ptMu1>25. && ptMu2>25. && !(mDiJet>300.0 && deltaEtaJets>3.0 && productEtaJets<0.0)";
 
    if (optionMap.count("sigFile")<1)
    {
